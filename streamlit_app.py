2e717bfdc12323b3c04a067419440bbb761a9bdf
luciams11 <114691984+luciams11@users.noreply.github.com>
2024-10-25 11:34:20 +0200

    Update streamlit_app.py
    Commenting API code

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,9 +38,9 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-        st.subheader(each_fruit + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        #st.subheader(each_fruit + ' Nutrition Information')
+        f#ruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit)
+        #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
